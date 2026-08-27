ef75ab2e600300ecc15c5c2f7573ab0be46f36ce
leemyl <leemyl2002@gmail.com>
2026-08-27 16:11:21 +0900

    chore: pr 테스트

--- src/main/java/com/example/meditation/MeditationApplication.java
+++ src/main/java/com/example/meditation/MeditationApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MeditationApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MeditationApplication.class, args);
 	}
