460df2549c2aed81445e52a51432fab5a56c17a9
jiyeonyooo <ji04230423@gmail.com>
2026-08-31 15:23:04 +0900

    chore: 의존성 관련 conflict 수정

--- src/main/java/com/example/meditation/MeditationApplication.java
+++ src/main/java/com/example/meditation/MeditationApplication.java
@@ -2,8 +2,14 @@ package com.example.meditation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.meditation", "room", "global"})
+@EntityScan(basePackages = "room.entity")
+@EnableJpaRepositories(basePackages = "room.repository")
 public class MeditationApplication {
 
 	public static void main(String[] args) {
--- src/main/java/global/dto/response/ApiResponse.java
+++ src/main/java/global/dto/response/ApiResponse.java
@@ -1,12 +1,28 @@
 package global.dto.response;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public record ApiResponse<T>(
         String code,
         String message,
         T data
 ) {
 
-    public static <T> ApiResponse<T> success(String message, T data) {
-        return new ApiResponse<>("OK", message, data);
+    public static <T> ResponseEntity<ApiResponse<T>> success(
+            HttpStatus status,
+            String message,
+            T data
+    ) {
+        ApiResponse<T> response = new ApiResponse<>(status.name(), message, data);
+        return ResponseEntity.status(status).body(response);
+    }
+
+    public static ResponseEntity<ApiResponse<Void>> error(
+            HttpStatus status,
+            String message
+    ) {
+        ApiResponse<Void> response = new ApiResponse<>(status.name(), message, null);
+        return ResponseEntity.status(status).body(response);
     }
 }
