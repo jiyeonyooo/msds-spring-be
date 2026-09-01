0755fc8a01a4a4bdf30f68f6ddeda7ab28ee04f0
JongIn-Leem <0422ll@snu.ac.kr>
2026-09-01 17:45:50 +0900

    fix: support reservation query parameter binding

--- src/main/java/resv/dto/AdminResvSearchRequestDTO.java
+++ src/main/java/resv/dto/AdminResvSearchRequestDTO.java
@@ -19,11 +19,23 @@ public class AdminResvSearchRequestDTO {
     @BindParam("resv_status")
     private ResvStatus resvStatus;
 
+    public void setResv_status(ResvStatus resvStatus) {
+        this.resvStatus = resvStatus;
+    }
+
     @BindParam("search_from_date")
     private LocalDate searchFromDate;
 
+    public void setSearch_from_date(LocalDate searchFromDate) {
+        this.searchFromDate = searchFromDate;
+    }
+
     @BindParam("search_to_date")
     private LocalDate searchToDate;
+
+    public void setSearch_to_date(LocalDate searchToDate) {
+        this.searchToDate = searchToDate;
+    }
 
     private String keyword;
 
@@ -31,7 +43,15 @@ public class AdminResvSearchRequestDTO {
     @BindParam("page_num")
     private Integer pageNum = 0;
 
+    public void setPage_num(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+
     @Min(value = 1, message = "페이지 크기는 1 이상이어야 합니다.")
     @BindParam("page_size")
     private Integer pageSize = 10;
+
+    public void setPage_size(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
 }
